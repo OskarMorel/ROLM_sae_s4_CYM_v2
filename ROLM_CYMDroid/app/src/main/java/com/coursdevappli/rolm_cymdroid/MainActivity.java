a3fdbb3058686020feff394b9b333ed00c64924e
MorelOskar <103489695+OskarMorel@users.noreply.github.com>
2023-03-28 16:42:12 +0200

    Authentification finie

--- ROLM_CYMDroid/app/src/main/java/com/coursdevappli/rolm_cymdroid/MainActivity.java
+++ ROLM_CYMDroid/app/src/main/java/com/coursdevappli/rolm_cymdroid/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     /** Etiquette pour les messages de log */
     private static final String TAG_LOG = "ACCES WEB";
 
-    private static String urlApi = "http://localhost/ROLM_sae_s4_CYM_v2/API_CheckYourMood/login?login";
+    private static String urlApi = "http://192.168.146.1/ROLM_sae_s4_CYM_v2/API_CheckYourMood/login?login=";
 
     private EditText editTextLogin, editTextPwd;
 
@@ -58,10 +58,10 @@ public class MainActivity extends AppCompatActivity {
         try {
             // le titre saisi par l'utilisateur est récupéré et encodé en UTF-8
             String login = URLEncoder.encode(editTextLogin.getText().toString(), "UTF-8");
-            String pwd = URLEncoder.encode(editTextLogin.getText().toString(), "UTF-8");
+            String pwd = URLEncoder.encode(editTextPwd.getText().toString(), "UTF-8");
 
             // le titre du film est inséré dans l'URL de recherche du film
-            String url = urlApi + "=" + login + "&pwd=" + pwd;
+            String url = urlApi + login + "&pwd=" + pwd;
             /*
              * on crée une requête GET, paramètrée par l'url préparée ci-dessus,
              * Le résultat de cette requête sera un objet JSon, donc la requête est de type
@@ -79,28 +79,17 @@ public class MainActivity extends AppCompatActivity {
                             } catch (JSONException e) {
                                 throw new RuntimeException(e);
                             }
-                            if (reponse != null && reponse.length() > 0) {
-                                int statusCode = 0;
-                                try {
-                                    statusCode = reponse.getInt("statusCode");
-                                } catch (JSONException e) {
-                                    throw new RuntimeException(e);
-                                }
-                                if (statusCode == 200) {
-                                    Intent intent = new Intent(MainActivity.this, Accueil.class);
-                                    // Démarrer l'activité de destination
-                                    startActivity(intent);
-                                } else {
-                                    //Toast.makeText(this, R.string.toast_erreur, Toast.LENGTH_LONG).show();
-                                }
-                            }
+                            Intent intent = new Intent(MainActivity.this, Accueil.class);
+                            // Démarrer l'activité de destination
+                            startActivity(intent);
                         }
                     },
                     // écouteur du retour de la requête si aucun résultat n'est renvoyé
                     new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError erreur) {
-
+                            erreur.printStackTrace();
+                            Toast.makeText(getApplication(), R.string.toast_erreur_login, Toast.LENGTH_LONG).show();
                         }
                     });
             // la requête est placée dans la file d'attente des requêtes
@@ -109,7 +98,5 @@ public class MainActivity extends AppCompatActivity {
             // problème lors de l'encodage de la chaîne titre
             Toast.makeText(this, R.string.toast_erreur, Toast.LENGTH_LONG).show();
         }
-
     }
-
 }
